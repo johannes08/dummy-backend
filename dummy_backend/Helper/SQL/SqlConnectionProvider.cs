610d0d2e95243ad05cf5765485cc35a2969f6ef4
johannes08 <johhuegel@gmail.com>
2021-02-26 15:04:06 +0100

    Add logger

--- dummy_backend/Helper/SQL/SqlConnectionProvider.cs
+++ dummy_backend/Helper/SQL/SqlConnectionProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MySqlConnector;
 
@@ -8,10 +9,12 @@ namespace dummy_backend.Helper.SQL
     public class SqlConnectionProvider : ISqlConnectionProvider
     {
         public readonly SqlCredentials _SqlCredentials;
+        public readonly ILogger _logger;
 
-        public SqlConnectionProvider(IOptions<SqlCredentials> sqlCredentials)
+        public SqlConnectionProvider(IOptions<SqlCredentials> sqlCredentials, ILoggerFactory loggerFactory)
         {
             _SqlCredentials = sqlCredentials.Value;
+            _logger = loggerFactory.CreateLogger<ISqlConnectionProvider>();
         }
 
         public async Task<MySqlConnection> GetConnection()
@@ -20,8 +23,13 @@ namespace dummy_backend.Helper.SQL
             {
                 throw new ArgumentException($"'[{_SqlCredentials.ConnectionString}]' ConnectionString was not found");
             }
+            
+            _logger.LogInformation($"Try to get mariadb connection: {_SqlCredentials.ConnectionString}");
+            
             var connection = new MySqlConnection(_SqlCredentials.ConnectionString);
             await connection.OpenAsync();
+            
+            _logger.LogInformation($"Connection Opened successfully: {_SqlCredentials.ConnectionString}");
 
             return connection;
         }
